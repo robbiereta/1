5134b2808b1013add9b896558a15a31e3af0692a
Robbie Reta <robbie.reta67@gmail.com>
2023-04-12 14:43:24 -0600

    .

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const result = excelToJson({
     sourceFile: '1.xlsx',
     sheets: [{
         name: 'Refacc  Kurazai-Carabela-Islo',
-        range: 'B11:E36710'
+        range: 'B11:H36710'
     }]
 });
  console.log(result)
+ var html_tablify = require('html-tablify');
+var options = {
+    data: [result]
+};
+var html_data = html_tablify.tablify(options);
+
+const main= document.getElementById('main')
