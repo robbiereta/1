2485e947d5a0f2eb4219943da0363034630e8a39
Roberto Reta <robbie.reta67@gmail.com>
2023-04-22 19:52:38 +0000

    Update 3 files

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-'use strict';
-const excelToJson = require('convert-excel-to-json');
- 
-const result = excelToJson({
-    sourceFile: '1.xlsx',
-    sheets: [{
-        name: 'Refacc  Kurazai-Carabela-Islo',
-        range: 'B11:H36710'
-    }]
-});
- console.log(result)
- var html_tablify = require('html-tablify');
-var options = {
-    data: [result]
-};
-var html_data = html_tablify.tablify(options);
+"use strict";
+const excelToJson = require("convert-excel-to-json");
 
-const main= document.getElementById('main')
+const result = excelToJson({
+  sourceFile: "1.xlsx",
+  sheets: [
+    {
+      name: "Refacc  Kurazai-Carabela-Islo",
+      range: "B11:H36710",
+    },
+  ],
+});
+console.log(result);
+const jsonfile = require("jsonfile");
+
+const file = "/tmp/data.json";
+
+jsonfile.writeFile(file, result, function (err) {
+  if (err) console.error(err);
+});
